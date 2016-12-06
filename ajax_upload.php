1be97f9ac2d24b587c9a531ed704b890f12e48a5
Mark Kirshner Chico <zhacarias.snizer@gmail.com>
2016-12-06 13:04:21 +0800

    Clean Code and Make it OOP Approach

--- ajax_upload.php
+++ ajax_upload.php
@@ -1,141 +1,247 @@
-<?php 
+<?php
+	/**==================================
+	* PROJECT   IMAGE RECOGNITION API
+	* FILE      ajax_uplaod.php
+	* VERSION   1.0
+	* AUTHOR    Mark Kirshner Chico
+	* EMAIL     zhacarias.snizer@gmail.com
+	* DATE      2016-01-06 UTC
+	* ==================================
+	* You can copy this source code for your project
+	* This is free to share
+	* Just give credits to the author.
+	* All rights reserved.
+	*/
+
 	header('Access-Control-Allow-Origin: *');
 
+	require_once('config.php');
 
-  if(isset($_FILES['file']['type'])) {
-      $imageurl = $_FILES['file']['name'];
-      $file = $_FILES['file']['tmp_name'];
-      $remote_file = 'images/'.$_FILES['file']['name'];
+	class SearchImage
+	{
+		/**
+		*  =======================
+		*	File Name
+		*  =======================
+		*/
+		private $filename;
 
-      //ftp
-      $ftp_server = 'raveteam.net';
-      $ftp_user_name = 'zhacarias@raveteam.net';
-      $ftp_user_pass = '#Tp(?ZWB3d;K';
-      // set up basic connection
-      $conn_id = ftp_connect($ftp_server) or die("Couldn't connect to $ftp_server");
-      // login with username and password
-      if(@$login_result = ftp_login($conn_id, $ftp_user_name, $ftp_user_pass)) {
-		  //if (ftp_chmod($conn_id, 664 , $file) !== false){
-			  // upload a file
-			  if (ftp_put($conn_id, $remote_file, $file, FTP_BINARY)) {
-			   //echo "successfully uploaded'.$file\n";
-			   $data = array('message' => 'Successfully uploaded','status' => 'completed');
-				if(file_exists($remote_file)) {
-					imageapi($data,$imageurl);
+		/**
+		*  =======================
+		*	Actual File Source / Path
+		*  =======================
+		*/
+		private $source;
+
+		/**
+		*  =======================
+		*	File Destination
+		*	Remote folder
+		*  =======================
+		*/
+		private $destination;
+
+		/**
+		*  =======================
+		*	FTP HOST / SERVER
+		*  =======================
+		*/
+		private $ftp_server;
+
+		/**
+		*  =======================
+		*	FTP USERNAME
+		*  =======================
+		*/
+		private $ftp_username;
+
+		/**
+		*  =======================
+		*	FTP PASSWORD
+		*  =======================
+		*/
+		private $ftp_password;
+
+		/**
+		*  =======================
+		*	FTP CONNECTION ID
+		*  =======================
+		*/
+		private $conn_id;
+
+
+
+		/**
+		*  =======================
+		*	Constructor
+		*  =======================
+		*/
+		public function __construct()
+		{
+			if (isset($_FILES['file']['type'])) {
+				// File Information
+				$this->filename = $_FILES['file']['name'];
+				$this->source = $_FILES['file']['tmp_name'];
+				$this->destination = IMAGEPATH . $this->filename;
+
+				// FTP Information
+				$this->ftp_server = FTP_SERVER;
+				$this->ftp_username = FTP_USER;
+				$this->ftp_password = FTP_PASS;
+
+				// Start Connection
+				return $this->ftp_con();
+			}
+		}
+
+		/**
+		*  =======================
+		*	Start Connection
+		*  =======================
+		*/
+		private function ftp_con()
+		{
+			$this->conn_id = ftp_connect($this->ftp_server) or die("Couldn\'t connect to $this->ftp_server");
+
+			if (ftp_login($this->conn_id, $this->ftp_username, $this->ftp_password)) {
+				
+				if (ftp_put($this->conn_id, $this->destination, $this->source, FTP_BINARY)) {
+					$data = array(
+						'message' 	=> 'Successfully uploaded',
+						'status' 	=> 'completed'
+					);
+
+					if (file_exists($this->destination)) {
+						return $this->camfindconfig($this->filename);
+					}
+				} else {
+					$data = array(
+						'message' 	=> 'There was a problem while uploading',
+						'status' 	=> 'not completed'
+					);
+					return $data;
 				}
-			  } else {
-			   $data = array('message' => 'There was a problem while uploading','status' => 'not completed');
-			  }
-		  //}else{ 
-			 //$data = array('message' => 'Can\'t set permission','status' => 'not completed');
-			 //echo json_encode($data);
-		  //}
-			  
-        }
-	  else{
-		  $data = array('message' => 'Can\'t login to the server ','status' => 'not completed');
-		  echo json_encode($data);
-	  }
-	  
-	  // close the connection
-	  ftp_close($conn_id);
-    }
-	
-	
-	
-	
-	
-	
-	function imageapi($data,$imagename){
-      $url = 'https://camfind.p.mashape.com/image_requests';
-  		$imageurl = 'http://raveteam.net/camfind/images/'.$imagename;
-  		$headers =
-  			array(
-  			"X-Mashape-Key : YWSjOzwzSdmshDNJFburlOu7cRr0p1GmXaIjsn8zu7PH28372G ",
-  			"Content-Type : application/x-www-form-urlencoded",
-  			"Accept : application/json");
-  		$fields =
-  			array(
-  			"focus[x]" => "480",
-  			"focus[y]" => "640",
-  			"image_request[altitude]" => "27.912109375",
-  			"image_request[language]" => "en",
-  			"image_request[latitude]" => "35.8714220766008",
-  			"image_request[locale]" => "en_US",
-  			"image_request[longitude]" => "14.3583203002251",
-  			"image_request[remote_image_url]" => $imageurl);
+				
+			} else {
+				$data = array(
+					'message' 	=> 'Can\'t login to the server ',
+					'status' 	=> 'not completed'
+				);
+				return $data;
+			}
 
-  		//url-ify the data for the POST
-  		$fields_string = "";
-  		foreach($fields as $key=>$value) {
-  			$fields_string .= $key.'='.$value.'&';
-  		}
-  		rtrim($fields_string, '&');
+			// close the connection
+	 		ftp_close($this->conn_id);
+		}
 
-  		//open connection
-  		$ch = curl_init();
+		/**
+		*  ==============================
+		*	Camfind Configuration Method
+		*	@param [$data] - array
+		*	@param [$imagename] - string
+		*  ==============================
+		*/
+		private function camfindconfig($imagename)
+		{
+			$url = CAMFIND_REQUEST;
+			$imageurl = BASEURL . IMAGEPATH . $imagename;
+			$headers = array(
+								"X-Mashape-Key 	: " .API_KEY ." ",
+								"Content-Type 	: application/x-www-form-urlencoded",
+								"Accept 		: application/json"
+							);
+			
+			$fields = array(
+								"focus[x]" 							=> "480",
+								"focus[y]"							=> "640",
+								"image_request[altitude]" 			=> "27.912109375",
+								"image_request[language]" 			=> "en",
+								"image_request[latitude]" 			=> "35.8714220766008",
+								"image_request[locale]" 			=> "en_US",
+								"image_request[longitude]" 			=> "14.3583203002251",
+								"image_request[remote_image_url]" 	=> $imageurl
+							);
+						
+			//url-ify the data for the POST
+			$fields_string = "";
+			foreach ($fields as $key=>$value) {
+				$fields_string .= $key .'='. $value .'&';
+			}
+			rtrim($fields_string, '&');
 
-  		//set the url, number of POST vars, POST data
-  		curl_setopt($ch,CURLOPT_URL, $url);
-  		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-  		curl_setopt($ch,CURLOPT_POST, count($fields));
-  		curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
-  		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-  		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-  		curl_setopt($ch, CURLOPT_MAXREDIRS, 10);
-  		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+			return $this->camfindrequest($url, $headers, $fields, $fields_string);
+		}
 
-  		//execute post
-  		$result = curl_exec($ch);
+		private function camfindrequest($url, $headers, $fields, $fields_string)
+		{
+			//open connection
+			$ch = curl_init();
 
-  		//close connection
-  		curl_close($ch);
+			//set the url, number of POST vars, POST data
+			curl_setopt($ch, CURLOPT_URL, $url);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_POST, count($fields));
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			curl_setopt($ch, CURLOPT_MAXREDIRS, 10);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
-  		$result = json_decode($result, true);
-  		//echo '<pre>',print_r($result),'</pre>';
-  		if($result['token'] != ""){
-  				//echo $result['token'] . '<br />';
+			//execute post
+			$result = curl_exec($ch);
 
-  				///////////////////////////////////
-  				// SECOND REQUEST
-  				///////////////////////////////////
+			//close connection
+			curl_close($ch);
 
-  				//set POST variables
-  				$url = "https://camfind.p.mashape.com/image_responses/" . $result['token'];
-  				//echo $url;
+			$result = json_decode($result, true);
 
-  				$headers = array(
-  				    'X-Mashape-Key: YWSjOzwzSdmshDNJFburlOu7cRr0p1GmXaIjsn8zu7PH28372G',
-  				    'Accept: application/json'
-  				); 
+			if($result['token'] != ""){
+				return $this->camfindresponse($result);
+			} else {
+				$data = array(
+					'message' 	=> 'Token not found.. Please try again !',
+					'status' 	=> 'not completed',
+					'imagename' => $imagename
+				);
+				return json_encode($data);
+			}
+		}
 
-  				$start = microtime(true);
-  				//open connection
-  				$ch = curl_init();
-  				do {
-  				    //set the url
-  				    curl_setopt($ch, CURLOPT_URL, $url);
-  				    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-  				    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-  				    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-  				    curl_setopt($ch, CURLOPT_MAXREDIRS, 10);
-  				    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-  				    curl_setopt($ch, CURLOPT_LOW_SPEED_TIME, 30);
- 
-  				    $result = json_decode(curl_exec($ch), true); 
+		private function camfindresponse($data, $result)
+		{	
+			$data = [];
+			$url = CAMFIND_RESPONSE . $result['token'];
 
-  				} while((number_format((microtime(true) - $start), 2) < 20) && $result['status'] == 'not completed');
-          $data['fruitname'] = $result['name'];
-          $data['fruitstatus'] = $result['status'];
+			$headers = array(
+				'X-Mashape-Key: '. API_KEY .' ',
+				'Accept: application/json'
+			);
+
+			$start = microtime(true);
+			
+			$ch = curl_init();
+			do {
+				curl_setopt($ch, CURLOPT_URL, $url);
+				curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+				curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+				curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+				curl_setopt($ch, CURLOPT_MAXREDIRS, 10);
+				curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+				curl_setopt($ch, CURLOPT_LOW_SPEED_TIME, 30);
+
+				$result = json_decode(curl_exec($ch), true); 
+
+			} while( (number_format((microtime(true) - $start), 2) < 20) && $result['status'] == 'not completed' );
+			
+			$data['objectname'] = $result['name'];
+			$data['objectstatus'] = $result['status'];
           
-  				//echo '<pre>',print_r($result),'</pre> <br />';
-  				//echo '<label> Fruit Name: </label> <code>'. $result['name'] . '</code>';
-  		  curl_close($ch);
+  		 	curl_close($ch);
 
-  		}
-  		else {
-			$data = array('message' => 'Token not found.. Please try again !','status' => 'not completed','imagename' => $imagename);
-  		}
-		echo json_encode($data);
-    }
-	
+			return json_encode($data);
+		}
+
+	}
+
+	// Start App
+	$app = new SearchImage();
---
+++ config.php
@@ -0,0 +1,53 @@
+<?php
+/**==================================
+* PROJECT   IMAGE RECOGNITION API
+* FILE      config.php
+* VERSION   1.0
+* AUTHOR    Mark Kirshner Chico
+* EMAIL     zhacarias.snizer@gmail.com
+* DATE      2016-01-06 UTC
+* ==================================
+* You can copy this source code for your project
+* This is free to share
+* Just give credits to the author.
+* All rights reserved.
+*/
+
+
+/**======================
+*   PROJECT URL
+*========================
+*/
+
+define('BASEURL', 'http://raveteam.net/camfind/');
+define('IMAGEPATH', 'images/');
+
+
+/**======================
+*   FTP ACCOUNT
+*========================
+*/
+
+// HOST
+define('FTP_SERVER','raveteam.net');
+
+// USERNAME
+define('FTP_USER', 'zhacarias@raveteam.net');
+
+// PASSWORD
+define('FTP_PASS', '#Tp(?ZWB3d;K');
+
+
+/**======================
+*   API CONFIG
+*========================
+*/
+
+// API KEY
+define('API_KEY', 'YWSjOzwzSdmshDNJFburlOu7cRr0p1GmXaIjsn8zu7PH28372G');
+
+// URL Image Request
+define('CAMFIND_REQUEST', 'https://camfind.p.mashape.com/image_requests');
+
+// URL Image Response
+define('CAMFIND_RESPONSE', 'https://camfind.p.mashape.com/image_responses/');
